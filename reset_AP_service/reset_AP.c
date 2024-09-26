78c374bc2f2efdbf7f93436034941cccef22efe8
joelson <joelsonrocha.eng@gmail.com>
2024-09-26 12:20:38 -0300

    adding new version of restart ap service

--- reset_AP_service/reset_AP.c
+++ reset_AP_service/reset_AP.c
@@ -1,8 +1,31 @@
 #include <stdio.h>    // Used for printf() statements
 #include <wiringPi.h> // Include WiringPi library!
 #include <stdlib.h>
-
+#include <stdbool.h>
 const int butPin = 17; // Active-low button - Broadcom pin 17, P1 pin 11
+char buf[1024];
+
+bool isAPDisabled(){
+	FILE *in = popen("ls /etc/netplan | grep AP.yaml.txt", "r");
+
+   if(in == NULL)
+	{
+		perror("popen:");
+		exit(EXIT_FAILURE);
+	}
+
+   // Write the output of the command to the console.
+	while(fgets(buf, sizeof(buf), in) != NULL)
+    //	printf("result: %s", buf);
+      if(buf[0]==0){
+	      printf("AP is enabled\n");
+          return false;
+      }
+      else{
+	      printf("AP is disabled\n");
+          return true;
+      }
+}
 
 
 int main(void)
@@ -13,11 +36,10 @@ int main(void)
     pinMode(butPin, INPUT);      // Set button as INPUT
     pullUpDnControl(butPin, PUD_UP); // Enable pull-up resistor on button
 
-    printf("Blinker is running! Press CTRL+C to quit.\n");
     int count=0;
     while(1)
     {
-        if (digitalRead(butPin)) // Button is released if this returns 1
+        if (digitalRead(butPin))
         {
             printf("Button is not pressed\n");
             if(count!=0)
@@ -25,12 +47,11 @@ int main(void)
                 count=0;
             }
         }
-        else // If digitalRead returns 0, button is pressed
+        else 
         {
             printf("Button is pressed\n");
             count++;
-            if(count==5){
-                //first step here is to verify which file is enabled
+            if(count==5 && isAPDisabled()){
                 printf("restarting to pi AP mode ...\n");
                 printf("changing file names ...\n");
                 system("sudo mv /etc/netplan/AP.yaml.txt /etc/netplan/AP.yaml");
